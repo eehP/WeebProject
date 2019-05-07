eaf5f3cd05bfc9d112094d403899987ffd2b2e21
eehP <fel.putinier@gmail.com>
2019-05-07 16:10:01 +0200

    Update accueilAuth
    Change format html to php
    
    Check logged or not

--- src/Auth/accueilAuth.php
+++ src/Auth/accueilAuth.php
@@ -1,5 +1,4 @@
-<!--accueilAuthent.html -->
-
+<!--accueilAuth.php -->
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,6 +8,17 @@
 </head>
 
 <body>
+
+<?php 
+session_start();
+	if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    echo "Welcome to the member's area, " . $_SESSION['username'] . "!";
+	} 
+	else {
+    echo "Please log in first to see this page.";
+	}
+?>
+
 <h1><strong> Bienvenue sur l'intranet  </strong> </h1> 
 <p>Bonjour et bienvenue sur le site de l'ecole !</p>
 
