22b005cee04c0febbc6fc451cf6e3cd463136c10
eehP <fel.putinier@gmail.com>
2019-05-13 12:45:36 +0200

    Authentication work
    Final touch to authent process

--- src/DBAuth/conn.php
+++ src/DBAuth/conn.php
@@ -14,11 +14,12 @@ if(count($_POST)>0) {
   $count  = mysqli_num_rows($result);
   if($count==0) {
     $message = "Invalid Username or Password!";
+    header('Location: /../Connexion.html');
   } else {
-    $_SESSION['loggedin'] = true;
-    $_SESSION['username'] = $username
-    header('Location: ./Auth/accueilAuth.php');
-    // $message = "You are successfully authenticated!";
+    //$_SESSION['loggedin'] = true;
+    //$_SESSION['username'] = $username
+    header('Location: ../Auth/accueilAuth.html');
+    $message = "You are successfully authenticated!";
   }
 }
 ?>
