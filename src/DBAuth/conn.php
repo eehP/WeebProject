d423714765fe36694ebb12802759cc3914186434
eehP <fel.putinier@gmail.com>
2019-05-14 13:23:21 +0200

    Redo conn.php

--- src/DBAuth/conn.php
+++ src/DBAuth/conn.php
@@ -6,9 +6,11 @@
 <body>
 
 <?php
-if(count($_POST)>0) {
-  $conn = mysqli_connect("localhost","root","root","WeebBase");
-  $result = mysqli_query($conn,"SELECT * FROM Table_utilisateur WHERE user='" . $_POST["username"] . "' and password = '". $_POST["password"]."'");
+if(count($_POST)>0) 
+{
+    $conn = mysqli_connect("localhost","root","root","WeebBase");
+  
+    $result = mysqli_query($conn,"SELECT * FROM Table_utilisateur WHERE user='" . $_POST["username"] . "' and pass = '". $_POST["password"]."'");
   $count  = mysqli_num_rows($result);
   if($count==0) {
     header('Location: ../Connexion.html');
