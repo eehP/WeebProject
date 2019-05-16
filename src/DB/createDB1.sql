0cb329d2856e41ac99d8dad6f5bad34293f819c0
eehP <fel.putinier@gmail.com>
2019-05-16 13:36:52 +0200

    Final push

---
+++ src/DB/createDB1.sql
@@ -0,0 +1,23 @@
+create database WeebBase character set 'UTF8';
+use WeebBase;
+
+CREATE TABLE IF NOT EXISTS `Table_utilisateur`( 
+
+`id` int(3) NOT NULL AUTO_INCREMENT, 
+
+`Nom` varchar(40) COLLATE latin1_general_ci NOT NULL,
+
+`Prenom` varchar(40) COLLATE latin1_general_ci NOT NULL,
+
+`Classe` varchar(3) COLLATE latin1_general_ci NOT NULL,
+
+`user`varchar(20) COLLATE latin1_general_ci NOT NULL, 
+
+`pass`varchar(40) COLLATE latin1_general_ci NOT NULL, 
+
+`nbr_connect` tinyint(1) NOT NULL DEFAULT 0,  
+
+PRIMARY KEY (`id`), 
+
+UNIQUE KEY `user` (`user`)
+);
